ca7c01e1aad421f08a74a904cec545e24764c2f9
gdsmith <george@mukuru.com>
2026-01-12 15:06:10 +0000

    Migrate to PSR-11

--- src/Codeception/Module/Mezzio.php
+++ src/Codeception/Module/Mezzio.php
@@ -9,9 +9,9 @@ use Codeception\Lib\Framework;
 use Codeception\Lib\Interfaces\DoctrineProvider;
 use Codeception\TestInterface;
 use Doctrine\ORM\EntityManagerInterface;
-use Interop\Container\ContainerInterface;
 use Mezzio\Application;
 use PHPUnit\Framework\AssertionFailedError;
+use Psr\Container\ContainerInterface;
 use Symfony\Component\BrowserKit\AbstractBrowser;
 
 /**
@@ -33,7 +33,7 @@ use Symfony\Component\BrowserKit\AbstractBrowser;
  * ## Public properties
  *
  * * application -  instance of `\Mezzio\Application`
- * * container - instance of `\Interop\Container\ContainerInterface`
+ * * container - instance of `\Psr\Container\ContainerInterface`
  * * client - BrowserKit client
  *
  */
